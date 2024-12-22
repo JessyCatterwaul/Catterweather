1ed596114032fb6739766353b909755fe291e500
Jessy Catterwaul <jessy@catterwaul.com>
2024-12-22 06:14:44 -0500

    Create Locations from search results

--- Catterweather/Location/Location.swift
+++ Catterweather/Location/Location.swift
@@ -1,3 +1,26 @@
-struct Location {
-  
+import Observation
+
+/// The location for which the user wants to know the weather.
+///
+/// See [Weather API's documentation](https://www.weatherapi.com/docs/#intro-location)
+/// for further available details.
+@Observable final class Location {
+  init(name: String, id: Int, currentWeather: Weather? = nil) {
+    self.name = name
+    self.id = id
+  }
+
+  let name: String
+
+  /// Weather API's unique identifier.
+  let id: Int
+
+  private(set) var currentWeather: Weather?
+}
+
+// MARK: - internal
+extension Location {
+  convenience init(_ searchResult: SearchResult) {
+    self.init(name: searchResult.name, id: searchResult.id)
+  }
 }
